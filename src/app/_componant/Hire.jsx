5158e839349eafb702b239311d758445d6687516
mohamed2233996 <medomano771@gmail.com>
2024-03-27 16:36:36 +0200

    v1.2

--- src/app/_componant/Hire.jsx
+++ src/app/_componant/Hire.jsx
@@ -6,7 +6,7 @@ const Hire = () => {
             <div className='w-full h-full absolute bg-primary opacity-70 top-0 left-0'></div>
             <div className='container m-auto flex justify-center items-center flex-col relative text-center'>
                 <h1 className='text-5xl text-white font-black my-6'>I am <span className='text-green-500'>Available</span> for freelancing</h1>
-                <p className='text-white mb-6 text-[18px]'>Available for any job, all you have to do is contact us to start immediately</p>
+                <p className='text-white mb-6 text-[18px]'>Available for any job &lsquo; all you have to do is contact us to start immediately</p>
                 <button className='text-white text-[18px] bg-primary rounded-full px-5 py-2 hover:bg-white hover:text-primary'>HIRE ME</button>
             </div>
         </div>
